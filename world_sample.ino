f0906b53ad08b2f5fd3f163409e8f3e56362f60d
Aidan Phillips <Aidan@aidan-phillipss-macbook.local>
2016-07-25 08:42:39 -0400

    world sample 1 save

--- world_sample.ino
+++ world_sample.ino
@@ -26,66 +26,66 @@ void world_sample() {
 
             case 0:   // NO TIME RESET
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                lineSegP3(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
-                          1000, 300, 1, 10, 1, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
+                         1000, 300, 1, 10, 1, .1, .25);
               }
               break;
 
             case 1:   // Same x/y pnoise mag
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                lineSegP3(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
-                          1000, 100, 0, 10, 10, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
+                         1000, 100, 0, 10, 10, .1, .25);
               }
               break;
 
             case 2:  // bigger difference in x/y pnoise mag
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                lineSegP3(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
-                          1000, 100, 0, 100, 1, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
+                         1000, 100, 0, 100, 1, .1, .25);
               }
               break;
 
             case 3:  //control with ink blots... change pen?  AND smaller noise scalar
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                lineSegP3(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
-                          1000, 100, 0, 10, 1, .1, .15);
+                lineSegP(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
+                         1000, 100, 0, 10, 1, .1, .15);
               }
               break;
 
             case 4: //change line spacing to .09
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                lineSegP3(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
-                          1000, 100, 0, 10, 1, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
+                         1000, 100, 0, 10, 1, .1, .25);
               }
               break;
 
             case 5: //some blank particals
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9 && random(0, 8) != 3) {
-                lineSegP3(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
-                          1000, 100, 0, 10, 1, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
+                         1000, 100, 0, 10, 1, .1, .25);
               }
               break;
 
             case 6: //the linear lines have an added noise component
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                y += (pnoise(lpx * 6, y * 6, _timeov / 10) / 20);
+                y += (pnoise(lpx * 6, y * 6, _timeov / 10) / 8);
                 _timeov++;
-                lineSegP3(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
-                          1000, 100, 0, 10, 1, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-10, 20) / 100, y + (double)random(-5, 10) / 100,
+                         1000, 100, 0, 10, 1, .1, .25);
               }
               break;
 
             case 7: //change the partical endpoint symmetry
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                lineSegP3(lpx, y, lpx + (double)random(-20, 20) / 100, y + (double)random(-20, 20) / 100,
-                          1000, 100, 0, 10, 1, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-20, 20) / 100, y + (double)random(-20, 20) / 100,
+                         1000, 100, 0, 10, 1, .1, .25);
               }
               break;
 
             case 8: //fast drawing speed
               if (pow((lpx - circle_c_col), 2) + pow((y - circle_c_row), 2) < 9) {
-                lineSegP3(lpx, y, lpx + (double)random(-20, 20) / 100, y + (double)random(-20, 20) / 100,
-                          400, 100, 0, 10, 1, .1, .25);
+                lineSegP(lpx, y, lpx + (double)random(-20, 20) / 100, y + (double)random(-20, 20) / 100,
+                         400, 100, 0, 10, 1, .1, .25);
               }
               break;
 
@@ -94,7 +94,7 @@ void world_sample() {
         }
       }
 
-      if (iteration_num == 3) {
+      if (iteration_num == 30) {
         lift_to(10, 27.5);
         double xbs = 10;
         double ybs = 27.5;
@@ -136,5 +136,5 @@ void world_sample() {
 
 
 /*
-  void lineSegP3(x1, y1, x2, y2, Speed, blot_time, time_zero, x_noise_mult, y_noise_mult, time_noise_mult, noise_scalar) {
+  void lineSegP(x1, y1, x2, y2, Speed, blot_time, time_zero, x_noise_mult, y_noise_mult, time_noise_mult, noise_scalar) {
 */
